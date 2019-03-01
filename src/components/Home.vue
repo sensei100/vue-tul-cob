8a99114ebf09ddcc86d976131aaeaa0982870e86
sensei100 <jenanton01@gmail.com>
2019-03-01 15:52:36 -0500

    Initial commit

---
+++ src/components/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="hello">
+    <Search />
+  </div>
+</template>
+
+<script>
+
+import Search from 'blacklight-vue/src/components/Search'
+
+
+export default {
+  name: 'Home',
+  components: {
+    Search
+  }
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,28 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Blacklight from 'blacklight-vue'
+import BlacklightRoutes from 'blacklight-vue/src/routes'
+import VueRouter from 'vue-router'
+import Home from './components/Home.vue'
 
 Vue.config.productionTip = false
 
+Vue.use(Blacklight)
+Vue.use(VueRouter)
+
+BlacklightRoutes.push({ path: '/', name: 'home', component: Home })
+const router = new VueRouter({
+  routes: BlacklightRoutes
+})
+
+Vue.http.options.root = 'http://demo.projectblacklight.org'
+Vue.http.options.headers = {
+  Accept: 'application/json'
+}
+
+Vue.prototype.$titleField = 'title_display'
+
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
